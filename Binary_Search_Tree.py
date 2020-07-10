854c41af96fad5e1102a7f8a70036a138e2b34da
Yeotae <dalo952003@gmail.com>
2020-07-10 21:04:53 +0900

    arrange

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -1,4 +1,4 @@
-class btnode:
+class Node:
     def __init__(self, data=None):
         self.data = data
         self.left = None
@@ -6,11 +6,11 @@ class btnode:
 
 class binary_search_tree(object):
     def __init__(self, data = None):
-        self.root = None
+        self.root = Node(data)
         self.num = 0
 
     def insert(self, data):
-        newnode = btnode(data)
+        newnode = Node(data)
         if self.root is None:
             self.root = newnode
             return
@@ -37,14 +37,15 @@ class binary_search_tree(object):
             self.postorder(cur.right)
             print(cur.data)
 
-test = binary_search_tree()
-test.insert(50)
-test.insert(30)
-test.insert(24)
-test.insert(5)
-test.insert(28)
-test.insert(45)
-test.insert(98)
-test.insert(52)
-test.insert(60)
-test.postorder(test.root)
+if __name__ == "__main__":
+    test = binary_search_tree()
+    test.insert(50)
+    test.insert(30)
+    test.insert(24)
+    test.insert(5)
+    test.insert(28)
+    test.insert(45)
+    test.insert(98)
+    test.insert(52)
+    test.insert(60)
+    test.postorder(test.root)
--- Level1-1.py
+++
@@ -1,5 +0,0 @@
-n = 118372
-answer = sorted(list(str(n)), reverse=True)
-answers = ""
-for i in answer:
-    answers += i
