e1444aa388c2287f0b763a52fbb1a44022d889c0
taeheon <dalo952003@gmail.com>
2020-10-08 19:58:12 +0900

    solve!!!!!!!!!

--- 1000_1500/1036.py
+++ 1000_1500/1036.py
@@ -1,57 +1,43 @@
-charn = [str(i) for i in range(10)] + [chr(i) for i in range(ord('A'), (ord('Z')+1))]
-i36 = {charn[i]:i for i in range(36)}
-del charn
-i36str = [input() for i in range(int(input()))]
-i36int = [[] for i in range(len(i36str))]
-for i in range(len(i36str)):
-    for j in range(len(i36str[i])):
-        i36int[i].append(i36[i36str[i][j]])
-del i36str
-maxlen = 0
-for i in range(len(i36int)):
-    if maxlen < len(i36int[i]):
-        maxlen = len(i36int[i])
-i36col = [[] for i in range(maxlen)]
+N=int(input())
+s=[list(map(lambda x: ord(x)-55 if ord(x)>=65 else int(x),input()[::-1])) for i in range(N)]
+K=int(input())
 
-for i in range(len(i36int)):
-    for j in range(len(i36int[i])):
-        i36col[j].append(i36int[i][len(i36int[i])-1-j])
-for i in i36col:
-    i.sort()
-i36col.reverse()
+nd={i:0 for i in sum(s,[])}
+for i in nd:
+    temp=[0 for i in range(max(map(len,s)))]
+    for j in s:
+        for k in range(len(j)):
+            if j[k] == i:
+                temp[k] += 35
+            else:
+                temp[k] += j[k]
+    j=0
+    while j < len(temp):
+        if temp[j] > 35:
+            if j+1 == len(temp):
+                temp.append(0)
+            temp[j+1] += temp[j]//36
+        temp[j]%=36
+        j+=1
+    nd[i] = temp[::-1]
 
-mininum = []
-k = int(input())
-a = 0
-b = 0
-while len(mininum) < k:
-    if i36col[a][b] in mininum:
-        b+=1
-        if b == len(i36col[a]):
-            a+=1
-            b =0
-        continue
-    else:
-        mininum.append(i36col[a][b])
+if len(nd)>=K:
+    nl=[sorted(nd.items(),key=lambda x:(len(x[1]),x[1]),reverse=True)[i][0] for i in range(K)]
+else:
+    nl = [sorted(nd.items(), key=lambda x: (len(x[1]), x[1]), reverse=True)[i][0] for i in range(len(nd))]
+ans = [0 for i in range(max(map(len,s)))]
+for i in s:
+    for j in range(len(i)):
+        if i[j] in nl:
+            ans[j] += 35
+        else: ans[j] += i[j]
 
-sum = [0 for i in range(maxlen)]
-for i in range(len(i36col)):
-    for j in range(len(i36col[i])):
-        if i36col[i][j] in mininum:
-            i36col[i][j] = 35
-        sum[i] += i36col[i][j]
-print(i36col)
-print(sum)
-sum.reverse()
-for i in range(len(sum)):
-    if i+1 < len(sum):
-        sum[i+1] +=  sum[i]//36
-        sum[i] %= 36
-    else:
-        sum.append(sum[i]//36)
-        sum[i] %= 36
-sum.reverse()
-print(sum)
-for s in sum:
-    result = [k for k, v in i36.items() if v == s]
-print(result)
+i=0
+while i<len(ans):
+    if ans[i] >35:
+        if i+1 == len(ans):
+            ans.append(0)
+        ans[i+1] += ans[i]//36
+    ans[i]%=36
+    i+=1
+print("".join(map(lambda x: chr(x+55) if x>=10 else str(x), ans[::-1])))
---
+++ 1000_1500/1036_2.py
@@ -0,0 +1,20 @@
+N=int(input())
+s=[list(map(lambda x:ord(x)-55 if ord(x)>=65 else int(x),input()[::-1])) for i in range(N)]
+K=int(input())
+d={i:0 for i in sum(s,[])}
+for i in d:
+    for j in s:
+        for k in range(len(j)):
+            if j[k]==i:d[i]+=35*(36**k)
+            else:d[i]+=j[k]*(36**k)
+if len(d)>=K:l=[sorted(d.items(),key=lambda x:x[1],reverse=True)[i][0] for i in range(K)]
+else:l=[sorted(d.items(),key=lambda x:x[1],reverse=True)[i][0] for i in range(len(d))]
+b=0
+for i in s:
+    for j in range(len(i)):
+        if i[j] in l:b+=35*(36**j)
+        else:b+=i[j]*(36**j)
+a=[]
+while b >= 36:a.append(b%36);b=b//36
+a.append(b)
+print("".join(map(lambda x: chr(x+55) if x>=10 else str(x),a[::-1])))
