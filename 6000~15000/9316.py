40bc8db7a661784ad8e8c1450324febef3c9fb08
taeheon <dalo952003@gmail.com>
2020-12-04 01:57:50 +0900

    Create 9316.py

---
+++ 6000~15000/9316.py
@@ -0,0 +1,8 @@
+def print_Hello_Judge(n:int):
+    for i in range(n):
+        print(f'Hello World, Judge {i+1}!')
+
+if __name__ == '__main__':
+    n=int(input())
+    print_Hello_Judge(n)
+    pass
