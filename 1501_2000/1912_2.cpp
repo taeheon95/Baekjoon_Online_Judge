e90d8ab07e0f5051820876850b3b58774e56c2a7
balcording <dalo952003@gmail.com>
2021-01-03 15:43:07 +0900

    refactoring 1912

--- 1501_2000/1912_2.cpp
+++ 1501_2000/1912_2.cpp
@@ -7,21 +7,16 @@ int num[100000] ={0};
 
 int main()
 {
-    int n, max;
+    int n, maxnum;
     cin>>n;
     for(int i=1; i<=n; i++) cin>>num[i];
     for(int i=1; i<=n; i++)
     {
-        if(dp[i-1] + num[i] > num[i]){
-            dp[i] = dp[i-1] + num[i];
-        }
-        else{
-            dp[i] = num[i];
-        }
+        dp[i] = max(dp[i-1], 0) + num[i];
     }
-    max = dp[1];
+    maxnum = dp[1];
     for(int i=2; i<=n; i++)
-        if(max<dp[i])
-            max = dp[i];
-    cout<<max;
+        if(maxnum<dp[i])
+            maxnum = dp[i];
+    cout<<maxnum;
 }
