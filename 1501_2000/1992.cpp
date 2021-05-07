d4b946f0875865d78e035893d13b3aef63d358d9
taeheon95 <dalo952003@gmail.com>
2021-05-07 17:49:28 +0900

    i'm idiot

--- 1501_2000/1992.cpp
+++ 1501_2000/1992.cpp
@@ -2,45 +2,46 @@
 #include<string>
 using namespace std;
 
-int images[65][65] = {};
+int images[65][65] = {0};
+string quad_tree(int x, int y, int n);
 
-int main(){
+int main() {
     int N;
-    cin>>N;
+    cin >> N;
     string line;
     string result;
 
-    for (int i = 0; i < N; i++){
-        cin>>line;
-        for (int j = 0; j < N; j++){
-            images[i][j] = line[i] -'0';
+    for (int i = 0; i < N; i++) {
+        cin >> line;
+        for (int j = 0; j < N; j++) {
+            images[i][j] = line[j] - '0';
         }
     }
-    
 
-    result = quad_tree(0, 0, N);
-    cout<<result;
+
+    result.append(quad_tree(0, 0, N));
+    cout << result;
 }
 
-string quad_tree(int x, int y, int n){
-    if(n == 1)
+string quad_tree(int x, int y, int n) {
+    if (n == 1)
         return to_string(images[x][y]);
 
     string result = "";
-    for (int i = x; i < x+n; i++){
-        for (int j = y; j < y+n; j++){
-            if(images[i][j] != images[x][y]){
-                result += "(";
-                result += quad_tree(x, y, n/2);
-                result += quad_tree(x+n/2, y, n/2);
-                result += quad_tree(x, y+n/2, n/2);
-                result += quad_tree(x+n/2, y+n/2, n/2);
-                result += ")";
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
+                if (images[i][j] != images[x][y]) {
+                    result.append("(");
+                    result.append(quad_tree(x, y, n / 2));
+                    result.append(quad_tree(x, y + n / 2, n / 2));
+                    result.append(quad_tree(x + n / 2, y, n / 2));
+                    result.append(quad_tree(x + n / 2, y + n / 2, n / 2));
+                    result.append(")");
 
-                return result;
+                    return result;
+                }
             }
         }
-    }
-    
+
     return to_string(images[x][y]);
 }
