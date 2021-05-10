c54a195f7571e7149f6b534c4c925592c9ea7efa
taeheon95 <dalo952003@gmail.com>
2021-05-10 11:19:07 +0900

    solve binary search problem

--- 1000_1500/1072.py
+++ 1000_1500/1072.py
@@ -1,17 +1,15 @@
+from math import floor
 x,y=map(int,input().split())
-z=y//x
+z=float(100*y/x)
 high=1000000000
 low=0
-while high>=low:
-    mid=(high+low)//2
-    if (y+mid)//x==z+1:
-        break
-    elif (y+mid)//x>z+1:
-        high=mid-1
-    else:
-        low=mid+1
-
-if high>=low:
-    print(mid)
+ans = -1
+if(z >= 99):print(-1)
 else:
-    print(-1)
+    while high>=low:
+        mid=(high+low)//2
+        if floor((y+mid)*100/(x+mid))>z:
+            high=mid-1
+        else:
+            low=mid+1
+    print(high+1)
