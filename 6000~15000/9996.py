9ae24d7a80cde1d3ec25f1610e91776923f7bd6d
taeheon95 <dalo952003@gmail.com>
2021-08-10 01:16:34 +0900

    solve 9996 using python and regular expression

--- 6000~15000/9996.py
+++ 6000~15000/9996.py
@@ -1,14 +1,10 @@
-n = int(input())
-checkstr = input()
-indexofstar = 0
-for i in range(len(checkstr)):
-    if checkstr[i] == "*":
-        indexofstar = i
-        break
+import re
+def solution():
+    n = int(input())
+    checkStr = input().split("*")
+    checkRE = re.compile(checkStr[0]+'\w*'+checkStr[1])
+    answer = ["DA" if checkRE.fullmatch(input()) else "NE" for _ in range(n)]
+    for i in range(n):print(answer[i])
 
-for i in range(n):
-    s = input()
-    if checkstr[0:indexofstar] == s[0: indexofstar] and checkstr[indexofstar+1:] == s[-1:len(s)-len(checkstr)+indexofstar:-1]:
-        print("DA")
-    else:
-        print("NE")
+if  __name__ == '__main__':
+    solution()
