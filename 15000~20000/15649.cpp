6c0c32cb7c349abfb403903f9649d4d03bb0b04c
taeheon95 <dalo952003@gmail.com>
2021-08-19 14:10:32 +0900

    add 15649 answer

---
+++ 15000~20000/15649.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+const int MAX = 9;
+
+int N, M;
+int arr[MAX];
+bool visited[MAX];
+
+void func(int cnt){
+    if(cnt == M) {
+        for(int i=0; i<M; i++)
+            cout<<arr[i]<<" ";
+        cout<<"\n";
+        return;
+    }
+
+    for(int i=1; i<N+1; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            arr[cnt] = i;
+            func(cnt+1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    cin>>N>>M;
+
+    func(0);
+    return 0;
+}
