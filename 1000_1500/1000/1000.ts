39e12a4f524ae51e650a6b8de39f1223ffae9574
taeheon95 <dalo952003@gmail.com>
2022-02-21 21:23:44 +0900

    add another solve of 1000

--- 1000_1500/1000/1000.ts
+++ 1000_1500/1000/1000.ts
@@ -1,4 +1,4 @@
 import * as fs from 'fs'
 
-const [a, b] = require("fs").readFileSync(0, 'utf8').toString().split(' ').map(v=>Number(v));
+const [a, b] = fs.readFileSync(0, 'utf8').toString().split(' ').map(v=>Number(v));
 console.log(a+b);
